faf91207a88d67ebd5425ab2ec75f8a3bec6eafa
Deepak Rizal <deepak2000rizal@gmail.com>
2025-10-01 19:20:29 +0530

    feat: track each tab focus time and log the time to the console

--- background.js
+++ background.js
@@ -1,14 +1,28 @@
+let currentTab = null;
+let startTime = null;
+
+function logCurrentTime() {
+  if (currentTab && startTime) {
+    const duration = Date.now() - startTime;
+
+    console.log(`you have stayed in that tab for ${duration / 1000} seconds`);
+  }
+}
+
 chrome.tabs.onActivated.addListener((activeInfo) => {
+  logCurrentTime();
+
   chrome.tabs.get(activeInfo.tabId, (tab) => {
-    console.log(tab);
+    currentTab = { tabId: tab.id, title: tab.title };
+    startTime = Date.now();
   });
 });
 
 chrome.windows.onFocusChanged.addListener((windowId) => {
   if (windowId === chrome.windows.WINDOW_ID_NONE) {
-    console.log(
-      "minimised chrome or chrome window switched(different chrome window switched!)"
-    );
+    logCurrentTime();
+    startTime = null;
+    currentTab = null;
   } else {
     // some window focused — get active tab there
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
