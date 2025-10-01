164644805c61f933e87e3a3ed67697e4f726bd26
Deepak Rizal <deepak2000rizal@gmail.com>
2025-10-01 17:29:02 +0530

    feat: capture activity when switching tabs or Chrome windows

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-console.log(chrome.tabs.local);
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    console.log(tab);
+  });
+});
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    console.log(
+      "minimised chrome or chrome window switched(different chrome window switched!)"
+    );
+  } else {
+    // some window focused — get active tab there
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      console.log(tabs);
+    });
+  }
+});
